4e151e2e45d86ec26cc341149b9219c8593568ef
Mahak Nigam <134001355+Mahaknigam1@users.noreply.github.com>
2026-05-03 20:39:46 +0530

    Add allowedHosts to Vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts:['stellar-inspiration-production-70cc.up.railway.app'],
     port: 5173,
     host: true,
   },
